3901cf84af0dfea3187eba6e9de36298becb9de8
Can17272 <44787376+Can17272@users.noreply.github.com>
2018-11-20 21:48:53 -0600

    Add files via upload

---
+++ common.py
@@ -0,0 +1,19 @@
+import kivy
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+
+class Header(BoxLayout): pass;
+class Body(BoxLayout):pass;
+class Footer(BoxLayout):pass;
+class Container(BoxLayout):pass;
+class Btn1(Button):pass;
+class Btn2(Button):pass;
+class Btn3(Button):pass;
+class Btn4(Button):pass;
+class Btn5(Button):pass;
+class Btn6(Button):pass;
+class Separator(BoxLayout):pass;
+class Paragraph(BoxLayout):pass;
+class Title1(Label):pass;
+class Title2(Label):pass;
