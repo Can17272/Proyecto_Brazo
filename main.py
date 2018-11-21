ac977b80253171372f79f3686b9d17594147aff7
Can17272 <44787376+Can17272@users.noreply.github.com>
2018-11-20 20:47:29 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import struct
 import time
 Config.set('graphics', 'width', 640)
 Config.set('graphics', 'height', 640)
-ser = serial.Serial(port='COM11', baudrate=9600, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE,
+ser = serial.Serial(port='COM11', baudrate=2400, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE,
         bytesize=serial.EIGHTBITS, timeout=0)
 
 
@@ -21,8 +21,8 @@ class MainWid(BoxLayout):
     text = StringProperty("ola k ")
     valorescrito = StringProperty()
     rec1 = []
-    rec2 = [1,2,3,4,5,6]
-    rec3 = [0,2,4,6,8,10]
+    rec2 = []
+    rec3 = []
     numero = StringProperty()
     #man1 = 0
     #man2 = 0
@@ -30,49 +30,52 @@ class MainWid(BoxLayout):
 #-------------------------------Mandar-------------------------------------------
     def mandar1(self):
         for man1 in self.rec1:
-            ser.write(man1)
-            print(man1)
-        rec1.clear()
+            ser.write(bytes([int(man1)]))
+            print(bytes([int(man1)]))
+        self.rec1.clear()
     def mandar2(self):
         for man2 in self.rec2:
-            ser.write(man2)
+            ser.write(bytes([int(man2)]))
             print(man2)
-        rec1.clear()
+        self.rec1.clear()
     def mandar3(self):
         for man3 in self.rec3:
-            ser.write(man3)
+            ser.write(bytes([int(man3)]))
             print(man3)
-        rec1.clear()
-#-------------------------------Recibir Datos-------------------------------------------
+        self.rec1.clear()
+#-------------------------------Recibir Datos-------------------------------------
 
     def Serial1(self):
         self.valorescrito=self.tpuerto1.text
         x=int(self.valorescrito, base=0)
-        x=x*10      
+       # x=x/0.1  
         ser.flushInput()
         
         while x>0:
-            time.sleep(0.1)
+            time.sleep(1)
+            b=ser.read(4)
+            #b=int(b,0)
             #print ("hola")
-            self.rec1.extend([ord(ser.read(4))])
+            self.rec1.extend(b)
             #self.rec1.extend([13,15,17])
+           
             x=x-1
         self.numero=("Datos Guardados")
         print(self.rec1)
-        ser.close()
+        
         
     def Serial2(self):
         self.valorescrito=self.tpuerto1.text
         y=int(self.valorescrito, base=0)
-        y=y*10
+        #y=y
         ser.flushInput()
         
         while y>0:
-            time.sleep(0.1)
+            time.sleep(1)
             print ("hola1")
-            a=ser.read(2)
-            a=ord(a)
-            self.rec2.extend([a])
+            a=ser.read(4)
+            #a=ord(a)
+            self.rec2.extend(a)
             #self.rec1.extend([13,15,17])
             y=y-1
         self.numero = ("Datos Guardados")
@@ -82,13 +85,14 @@ class MainWid(BoxLayout):
     def Serial3(self):
         self.valorescrito=self.tpuerto1.text
         z=int(self.valorescrito, base=0)
-        z=z*10        
+        #z=z*10        
         ser.flushInput()
         
         while z>0:
-            time.sleep(0.1)
-            print ("hola2")
-            self.rec1.extend([ser.read(4)])
+            time.sleep(1)
+            c=ser.read(4)
+            #print ("hola2")
+            self.rec1.extend(c)
             #self.rec1.extend([13,15,17])
             z=z-1
         self.numero=("Datos Guardados")
